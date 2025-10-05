c935a4ca45a4e066b6a60fe5c48f23b05ff8d067
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-10-05 19:31:33 +0900

    Update some files

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 SHELL := /bin/bash
 
 install:
-	pip install torch transformers yasem
+	pip install torch transformers yasem fugashi unidic_lite
 
 activate:
 	source .venv/bin/activate
 
 venv:
 	python3 -m venv .venv
+
+hello:
+	python3 hello.py
--- hello.py
+++ hello.py
@@ -4,10 +4,19 @@ model_name = "hotchpotch/japanese-splade-v2"
 embedder = SpladeEmbedder(model_name)
 
 query = "車の燃費を向上させる方法は？"
-docs = [
+sentences = [
     "急発進や急ブレーキを避け、一定速度で走行することで燃費が良くなります。",
     "車の運転時、急発進や急ブレーキをすると、燃費が悪くなります。",
     "車を長持ちさせるには、消耗品を適切なタイミングで交換することが重要です。",
 ]
 
-print(embedder.rank(query, docs, return_documents=True))
+embeddings = embedder.encode(sentences)
+
+for i, emb in enumerate(embeddings):
+    print(sentences[i])
+    
+    token_values = embedder.get_token_values(emb)
+    for token, score in token_values.items():
+        print(f"{token:<15}: {score:.4f}")
+    
+    print()
